16b5a87baa18ded5fb48701079dabfec84328705
MedhanieG <medhanieug@gmail.com>
2021-11-22 16:25:49 -0700

    Update pizza.provider.ts

--- packages/server/src/application/providers/pizzas/pizza.provider.ts
+++ packages/server/src/application/providers/pizzas/pizza.provider.ts
@@ -1,7 +1,7 @@
-import { ObjectId, Collection } from 'mongodb';
+import { Collection } from 'mongodb';
 import { PizzaDocument, toPizzaObject } from '../../../entities/pizza';
 import { Pizza } from './pizza.provider.types';
-import validateStringInputs from '../../../lib/string-validator';
+
 
 class PizzaProvider {
   constructor(private collection: Collection<PizzaDocument>) {}
